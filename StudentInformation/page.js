44609eb92a01b89f4b3048c09696e307ce23de36
Badhan Talukder <122679578+badhan2001@users.noreply.github.com>
2024-03-09 01:03:34 +0600

    Add files via upload

---
+++ StudentInformation/page.js
@@ -0,0 +1,14 @@
+export default function StudentInformation(){
+    return(
+        <main className="mt-10">
+        <div>
+            <li>
+                Sadman Sami Khan<br/>ID-21701081<br/>Mobile-<br/>Email-
+            </li>
+            <li>
+                Badhan Talukder<br/>ID-21701079<br/>Mobile-<br/>Email-
+            </li>
+        </div>
+        </main>
+    );
+}
